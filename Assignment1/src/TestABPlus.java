b7f202715af7b771b0df7e7a8e976e1fa43ae57b
Lili Peng <lili.peng@venafi.com>
2018-01-20 20:49:23 -0700

    Updated the way to check whether a word is in the language for abplus

--- Assignment1/src/TestABPlus.java
+++ Assignment1/src/TestABPlus.java
@@ -12,42 +12,32 @@ public class TestABPlus {
 		
 		String input = args[0];
 		String result = "X = " + input + "\t\t\t Member?\t ";
-		result = (isABPlus(input) ? result + "true" : result + "false");
+		result = (isABPlus(input, false) ? result + "true" : result + "false");
 		System.out.println(result);
 	}
 	
-	public static boolean isABPlus(String s)
+	public static boolean isABPlus(String s, boolean valid)
 	{
+		// Base case
 		if (s.equals("a") || s.equals("b"))
 			return true;
 		
-		if (s.length() > 3)
-		{
-			if (s.startsWith("a+b") || s.startsWith("b+a") || s.startsWith("a+a") || s.startsWith("b+b"))
-				return isABPlus(s.substring(3));
-			
-			if (s.endsWith("a+b") || s.endsWith("b+a") || s.endsWith("a+a") || s.endsWith("b+b"))
-				return isABPlus(s.substring(0, s.length() - 3));
-		}
+		if (s.charAt(0) != 'a' && s.charAt(0) != 'b' && s.charAt(0) != '+')
+			return false;
 		
-		if (s.length() > 2)
-		{
-			if (s.startsWith("ab") || s.startsWith("ba"))
-				return isABPlus(s.substring(2));
-			if (s.endsWith("ab") || s.endsWith("ba"))
-				return isABPlus(s.substring(0, s.length() - 2));
-		}
+		if (valid == false && s.charAt(0) == '+')
+			return false;
 		
-		if (s.length() > 1) 
-		{
-			if (s.charAt(0) == 'a' || s.charAt(0) == 'b')
-				return isABPlus(s.substring(1));
-			
-			if (s.charAt(s.length() - 1) == 'a' || s.charAt(s.length() - 1) == 'b')
-				return isABPlus(s.substring(0, s.length() - 1));
-		}
+		if (s.charAt(s.length() - 1) == '+' && s.length() == 1)
+			return false;
 		
-		return false;
+		if (s.charAt(0) == 'a' || s.charAt(0) == 'b')
+			valid = true;
+		
+		if (s.charAt(0) == '+')
+			valid = false;
+		
+		return isABPlus(s.substring(1), valid);			
 	}
 
 }
